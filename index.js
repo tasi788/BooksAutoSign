b63f332ca5bb4baef1797440b60b13c2dc9f7f75
tasi788 <tdc@tdccc.com.tw>
2021-11-18 16:04:13 +0800

    🐛 修正 typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // cookie = 貼上
 let text = "{";
 for (i=0;i<cookie.length;i++) {
-    text+="\""+a[i].name+"\": \""+a[i].value+"\",\n"
+    text+="\""+cookie[i].name+"\": \""+cookie[i].value+"\",\n"
 }
 text += "}"
 console.log(text)
